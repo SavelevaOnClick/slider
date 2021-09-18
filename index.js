c938f1c0a6266e995f74e2a9736010d1a82e345f
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-09-18 18:08:34 +0300

    add first version adaptive

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 const arrowLeft = document.getElementById("arrow-left");
 const arrowRight = document.getElementById("arrow-right");
 const slider = document.querySelector(".slider");
+const sliderSection = document.getElementById("slider-section");
+const slides = document.querySelectorAll(".slid");
 
 function goToRight() {
-  Array.from(document.querySelectorAll(".slid")).forEach((slid) => {
+  slides.forEach((slid) => {
     slid.style.order =
-      +getComputedStyle(slid).order !== 8
-        ? +getComputedStyle(slid).order + 1
+      parseInt(getComputedStyle(slid).order) !== 8
+        ? parseInt(getComputedStyle(slid).order) + 1
         : 1;
   });
 }
 
 function goToLeft() {
-  Array.from(document.querySelectorAll(".slid")).forEach((slid) => {
+  slides.forEach((slid) => {
     slid.style.order =
-      +getComputedStyle(slid).order !== 1
-        ? +getComputedStyle(slid).order - 1
+      parseInt(getComputedStyle(slid).order) !== 1
+        ? parseInt(getComputedStyle(slid).order) - 1
         : 8;
   });
 }
+function adaptiveWidthOfSlidMini(slidNumber) {
+  slides.forEach((item) =>
+    Object.assign(item.style, {
+      flexBasis: `${
+        (sliderSection.offsetWidth -
+          (sliderSection.offsetWidth / 100) * (slidNumber * 4 + 2)) /
+        slidNumber
+      }px`,
+      height: `${
+        (sliderSection.offsetWidth -
+          (sliderSection.offsetWidth / 100) * (slidNumber * 4 + 2)) /
+        slidNumber
+      }px`,
+    })
+  );
+}
 
+function createWidthAndHeight() {
+  adaptiveWidthOfSlidMini(innerWidth < 769 ? 2 : 4);
+}
+createWidthAndHeight();
 arrowRight.addEventListener("click", (event) => goToRight());
 arrowLeft.addEventListener("click", (event) => goToLeft());
+
+window.addEventListener("resize", createWidthAndHeight);
